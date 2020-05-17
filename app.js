a91783b0b3c8aa8f3d289de18583fddce0792334
varvind7 <varvind7@gmail.com>
2020-05-17 13:15:05 -0400

    mongodb done

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const  db=require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -14,14 +15,26 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('5ec018237a23d011a174c0d6')
+    .then(user => {
+      req.user = new User(user.name,user.email,user.cart,user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+  
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+
+mongoConnect(()=> {
+ 
+  app.listen(3000);
+})
--- util/database.js
+++ util/database.js
@@ -1,10 +1,28 @@
-const mysql = require('mysql2');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user: 'root',
-    database:'nodejs',
-    password:'root'
-});
+let _db;
 
-module.exports=pool.promise();
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://arvind:1234@cluster0-fucio.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(client=> {
+    console.log('Connected');
+    _db= client.db();
+    callback();
+  }).catch(err => {
+    console.log(err);
+    throw err;
+  });
+
+};
+
+const getDb =() => {
+  if(_db) {
+    return _db;
+  }
+  throw 'no db found';
+}
+
+exports.mongoConnect=mongoConnect;
+exports.getDb=getDb;
+
